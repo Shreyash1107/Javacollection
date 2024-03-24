8de3e7102f762851d186a0c64c9ac6d40a7f1ae1
Shreyash Kawane <shreyunkawane@gmail.com>
2024-03-24 09:29:13 +0530

    Updated code

--- Vector/src/Capacity/VectorcrosscapacityApp.java
+++ Vector/src/Capacity/VectorcrosscapacityApp.java
@@ -1,6 +1,4 @@
 package Capacity;
-
-import java.security.Signer;
 import java.util.Scanner;
 import java.util.Vector;
 
@@ -11,5 +9,19 @@ public class VectorcrosscapacityApp
         Vector v = new Vector();   
         int limit;
         Scanner xyz = new Scanner(System.in);
+        int capacity = v.capacity();
+        System.out.println("Initial Capacity of a Vector is:" + capacity);
+        System.out.println("Enter the Limit for Vector Elements:");
+        limit = xyz.nextInt();
+        int i;
+        System.out.println("Enter the Data inside Vector:");
+        for(i=0;i<limit;i++)
+        {
+            int val = xyz.nextInt();
+            v.add(val);
+        }
+        System.out.println("Vector Elements are:" + v);
+        System.out.println("Size of Vector is:" + v.size());
+        System.out.println("After Capacity Cross, Vector Capacity is:" + v.capacity());
     }
 }
